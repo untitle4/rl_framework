6799fd400e0b7d3c548d3d24c8f16225c84b4197
Geyu Lin <geyu.lin@agoda.com>
2022-10-10 21:12:38 +0800

    add test file

---
+++ test/sample_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4
+
+
+def f():
+    raise SystemExit(1)
+
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
+
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
