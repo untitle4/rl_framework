83b7c55f6e997e74704733f7be903837fb48ef23
Lingy12 <e0376918@u.nus.edu>
2022-10-22 10:13:09 +0800

    add shell script

---
+++ run_experiment.sh
@@ -0,0 +1,5 @@
+python train.py --ename ./experiments/thompson_run_0 --lconfig ./configs/learning_config.json
+
+python train.py --ename ./experiments/upper_confidence_bound_run_0 --lconfig ./configs/learning_config_ucb.json
+
+python train.py --ename ./experiments/epsilon_greedy_run_0 --lconfig ./configs/learning_config_ep.json
